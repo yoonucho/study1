de0c19666d07ec26c98005a467f3f8543c0cabcc
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-11-19 22:40:04 +0900

    171119

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+	//gnbOpen();
+	gnbHover();
 	tab();
 });
+
+function gnbOpen() {
+      $(".gnb > li").on("mouseenter",function(){
+        $(".header_wrap").stop().animate( {height:355},500);
+      })
+      $(".header_wrap").on("mouseleave",function(){
+        $(this).stop().animate({height:122},500);
+      })
+
+}
+//    gnb메뉴 마우스이벤트 클릭시 sub메뉴 보이기
+
+
+function gnbHover() {
+	$(".gnb > li > a").on('mouseenter focusin', function(){
+		$(this).parent("li").addClass("hover").siblings("li").removeClass("hover");
+	});
+}
+
+
+
 
 function tab() {
 	$(".tab>li").on("click", function () {
