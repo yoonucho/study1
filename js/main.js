3cc7d5b5d334924da29b95013710dc8e72120b8b
tomorrowcho <yoonu78@naver.com>
2017-11-19 12:09:11 +0900

    171118

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 $(document).ready(function(){
-	tab1();
-	//tab2();
+	tab();
 });
 
-function tab1() {
-	var $tab1 = $("#tab1>li>a");
-	$tab1.on("click", function () {
-		if (!$(this).hasClass("on")) {
-			$tab1.removeClass("on").next().hide();
-			//$tab.next().hide();
-		}
-		$(this).addClass("on").next().show();
-		//$(this).next().show();
-
+function tab() {
+	$(".tab>li").on("click", function () {
+		 $(this).addClass("on").siblings().removeClass("on");
 	});
 
 }
 
-function tab2() {
-	var $tab2 = $("#tab2>li>a");
-	$tab2.on("click", function () {
-		if (!$(this).hasClass("on")) {
-			$tab2.removeClass("on").next().hide();
-			//$tab.next().hide();
-		}
-		$(this).addClass("on").next().show();
-		//$(this).next().show();
-
-	});
-
-}
