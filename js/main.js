8e7515f6cc4bc665022e24da17959c907b3591ac
tomorrowcho <yoonu78@naver.com>
2017-11-22 10:55:58 +0900

    171122

--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
 $(document).ready(function(){
 	gnbOpen();
-	gnbHover();
+	//gnbHover();
 	tab();
 });
 
 function gnbOpen() {
-<<<<<<< HEAD
-      $(".gnb >li>a").on("mouseenter focus",function(){
-=======
-      $(".gnb > li").on("mouseenter",function(){
->>>>>>> 428ace8a16d3ebee7f4b02e0f86eae213ddcd927
+      $(".gnb >li>a").on("mouseenter focusin",function(){
         $(".header_wrap").stop().animate( {height:355},500);
+//    gnb메뉴 마우스이벤트 클릭시 sub메뉴 보이기
+        $(this).parent("li").addClass("hover").siblings("li").removeClass("hover");
       })
       $(".header_wrap").on("mouseleave",function(){
         $(this).stop().animate({height:122},500);
       })
 
 }
-//    gnb메뉴 마우스이벤트 클릭시 sub메뉴 보이기
 
-
-function gnbHover() {
-	$(".gnb > li > a").on('mouseenter focusin', function(){
-		$(this).parent("li").addClass("hover").siblings("li").removeClass("hover");
-	});
-}
 
 
 
