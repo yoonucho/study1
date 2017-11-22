776304ac3bc8081b314b7fb7a3a790d6c6dbb214
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-11-22 13:32:32 +0900

    171122

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
 	gnbOpen();
-	//gnbHover();
+	searchOpen();
 	tab();
 });
 
+// gnb메뉴 마우스이벤트 
 function gnbOpen() {
       $(".gnb >li>a").on("mouseenter focusin",function(){
         $(".header_wrap").stop().animate( {height:355},500);
-//    gnb메뉴 마우스이벤트 클릭시 sub메뉴 보이기
-        $(this).parent("li").addClass("hover").siblings("li").removeClass("hover");
+       
       })
-      $(".header_wrap").on("mouseleave",function(){
-        $(this).stop().animate({height:122},500);
+        $(".header_wrap").on("mouseleave",function(){
+          $(this).stop().animate({height:122},500);
       })
+}
 
+// 검색이벤트
+function searchOpen() {
+  $(".search .search_icon").on("click focusin", function () {
+         // if($(this).hasClass("change"))
+
+              //$(this).removeClass("change");
+         // else
+             // $(this).addClass("change");
+              //$(this).next().slideToggle();
+         //$(this).next().slideToggle();
+         $(this).toggleClass("change").next().slideToggle();
+      });
 }
 
 
 
-
-
+// 탭
 function tab() {
 	$(".tab>li>a").on("click focusin", function () {
-		 $(this).parent("li").addClass("on").siblings().removeClass("on");
+		 $(this).parent().addClass("on").siblings().removeClass("on");
 	});
 
 }
