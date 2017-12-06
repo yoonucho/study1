adecad793a2ae3131cc110dcc0338f3ee86507d1
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-12-06 14:50:39 +0900

    171206_1450 코드정리

--- study1/js/main.js
+++ study1/js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
-	gnbOpen();
-  subClose();
-	searchOpen();
-	tab();
+  	gnbOpen();
+    subClose();
+  	searchOpen();
+    searchClose();
+  	tab();
 });
 
 // gnb메뉴 마우스이벤트 
-function gnbOpen() {
-      
+function gnbOpen() {     
   $(".gnb >li>a").on("mouseenter focusin",function(){
-    $(".header_wrap").stop().animate( {height:355},500);
-   
+    $(".header_wrap").stop().animate( {height:355},500); 
   })
 
   $(".header_wrap").on("mouseleave",function(){
@@ -18,25 +17,19 @@ function gnbOpen() {
     $(this).stop().animate({height:122},500);
   })
 }
-
-
-
 // 탭키이동  서브메뉴 마지막이벤트 
 function subClose(){
-  $(".gnb li:last-child .sub_wrap li:last-child>a").on ("focusin",function(){
+  $(".gnb li:last-child .sub_wrap li:last-child>a").on ("focusout",function(){
    $(".header_wrap").stop().animate({height:122},500);
   })
-
-
 }
 
 
 // 검색이벤트
 function searchOpen() {
-  $(".search .search_icon").on("click keyup", function () {
+  $(".search .icon_search").on("click keyup", function () {
      $(this).toggleClass("change").next().slideToggle();
-     
-     
+     // if문 사용시
      // if($(this).hasClass("change")){
      //    $(this).removeClass("change").next().slideUp();
      // //    $(this).next().slideUp();
@@ -47,10 +40,15 @@ function searchOpen() {
      // //     $(this).next().slideDown();
      // //     $(this).addClass("change");
      // }
-
   });
 }
-
+//탭키이동 검색창 닫기 
+function searchClose(){
+  $(".search .btn_search").on("click focusout",function(){
+    $(".search_wrap").slideUp();
+    $(".search .icon_search").removeClass("change");  
+  })
+}
 
 
 // 탭
@@ -58,6 +56,5 @@ function tab() {
 	$(".tab>li>a").on("click focusin", function () {
 	 $(this).parent().addClass("on").siblings().removeClass("on");
 	});
-
 }
 
--- study2/js/mobile.js
+++ study2/js/mobile.js
@@ -1,55 +1,43 @@
 $(document).ready(function(){
-  selectMenu();
-	
-  bookmarkToggle();
-	//tab();
+    selectMenu();
+  	bookmarkToggle();
 });
-
-
 
 // 셀렉트박스 이벤트
 function selectMenu() {
   $(".header h1").on("click", function () {
-    if($(this).hasClass("on")){
-        $(this).removeClass("on").next().slideUp();
-     }
-     else{
-        $(this).addClass("on").next().slideDown();
-     } 
+    // 만약 h1이 on클래스를 가지고 있으면  on클래스를 없애주고 슬라이드를 올려줘라
+    //그렇지 않으면 on클래스를 추가해주고 슬라이드를 내려줘라.
+      if($(this).hasClass("on")){
+          $(this).removeClass("on").next().slideUp();
+      }
+      else{
+          $(this).addClass("on").next().slideDown();
+      } 
      $(".sub_lst li").on("click",function(){
+      //.keywords에 .sub_lst li내용을 담아줘라.(ex:<a href="#">트렌드</a>)
          $(".header h1>.keywords").html($(this).html());
           $(".sub_lst").stop().slideUp();
           $("h1").removeClass("on");
-     })
-      
+     }) 
   });
 }
 
 
-// 문제점 온상태에서 아무것도 하지 않고 클릭할때 닫히면서 화살표도 바뀌어야 한다
+
 // 다른 컨텐츠를 선택했을때 자동으로 닫혀야 한다.
 // modalP.on( "click", function() {
 //     gnb.removeClass('on').stop().animate({left:-260},500);
 //     modalP.fadeOut(500);
      
+
 // 북마크 토글클래스
 function bookmarkToggle() {
   $(".bookmark .icon_bookmark").on("click", function () {
      $(this).toggleClass("on");
-
   });
 }
 
 
-// 탭
-// function tab() {
-// 	$(".sub_tab>li>a").on("click", function () {
-//     $(this).parent().addClass("on").siblings().removeClass("on");
-//      var activeTab = $(this).attr('href');
-//     $(".lst_wrap>.sub_tab_lst:visible").hide().css({"transition": "all .75s ease"});
-//     $(activeTab).show();
-//     return false;	
-// 	});
 
-// }
 
