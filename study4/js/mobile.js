964a75cca01fcf33f4e4b70a515b452a96214c40
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-12-21 12:07:37 +0900

    171221_1207

--- study4/js/mobile.js
+++ study4/js/mobile.js
@@ -1,41 +1,92 @@
 $(document).ready(function(){
+    menuOpen();
+    airplaneGo();
+    sub_imgOn();
     //bxSlider();
-    //selectMenu();
    
-    //lstClose();
 });
  
+ // 페이지 사이드 롤링
+// function bxSlider{
+//   $(".bxslider").bxSlider({
+
+//   })
+
+// }
 // 사이드랩 메인메뉴  클릭이벤트
-function selectMenu() {
-  $(".header h1").on("click", function () {
-    // 만약 h1이 on클래스를 가지고 있으면  on클래스를 없애주고 슬라이드를 올려줘라
-    //그렇지 않으면 on클래스를 추가해주고 슬라이드를 내려줘라.
-      if($(this).hasClass("on")){
-          $(this).removeClass("on").next().slideUp();
+function menuOpen() {
+  $(".btn_all").on("click", function () {
+      
+      if($(".side_wrap").hasClass("on")){
+           $(".side_wrap").removeClass("on");
+           $(".wrap").css ({"left":"0"})
       }
-      else{
-          $(this).addClass("on").next().slideDown();
-      } 
+      else{  
+          $(".side_wrap").addClass("on");
+          $(".wrap").css ({"left":"-124%"})
+      }
     
   });
 }
 
+function airplaneGo(){
+  $(".nav .gnb>li>a").on("click",function (){
+       $(this).parent().addClass("on").siblings().removeClass("on")
+
+       // if문 사용시
+
+     
+     // if($(this).hasClass("on")){
+     //     $(".nav .gnb>li").removeClass("on");
+         // $(this).find("[class$='s']").removeClass("on");
+         // $(".nav .gnb>li>a").find(".sp_img").removeClass("on");
+         // $(".nav .gnb>li>a").find("[class^='icon_']").css('background-color', 'red')
+         
+   
+     // }
+     // else{
+     // //    // $(this).parent().addClass("on");
+     //    $(this).find(".sp_img").addClass("on");
+     
+  
+     // }
+
+   
+    // $(this).find("[class$='s']").addClass("on").siblings().removeClass("on")
+
+  })
+}
+
+function sub_imgOn(){
+  $(".nav .gnb>li").on("click",function(){
+
+       var index = $(this).find(".sp_img").index();
+        
+   if ($(this).hasClass("on")) {
+       $(".sp_img").removeClass("on");
+          
+      }
+    else{
+         
+        $(this).eq(index).addClass("on");
+    } 
+  })
+
+
+  
+     
+
+
+    
+  //비행기가 보일때 안보일때로 
+
+}
 //btn_all을 클릭했을때 사이드랩을 보여줘라
-  //바로 하단에 있으니 .next()
+  
 //해당 리스트를 클릭했을때 on클래스를 추가해주고 텍스트효과와 비행기를 보여줘라
  
-// 다른 영역을 선택했을때  닫아줘라
-/*
-function lstClose() {
-  $(document).mouseup(function (e){
-    var $header = $(".header");
-    if( $header.has(e.target).length === 0){
-      $(".sub_lst").stop().slideUp();
-     $("h1").removeClass("on");
-    }
-  });
-}
-*/
+
+
  
 
  
