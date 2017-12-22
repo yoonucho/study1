4225f5491d8156e048362a4779f978ab787f8e75
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-12-22 18:30:42 +0900

    171222_1830 오앙다했다 이제 체인공부를 해보자!

--- study4/js/mobile.js
+++ study4/js/mobile.js
@@ -1,29 +1,27 @@
 $(document).ready(function(){
     menuOpen();
     airplaneGo();
-    //sub_imgOn();
+    quickNav();
     bxSlider();
    
 });
  
- // 페이지 사이드 롤링
+ // bxSlider 롤링
 function bxSlider(){
   var slider= $(".bxslider").bxSlider({
       infiniteLoop:false,
-      hideControlOnEnd:true
+      hideControlOnEnd:true,
+      touchEnabled:false
   });
 
-
-
-
    // "다음" 버튼 클릭시 동작 정의
-       $('.bx-next').click(function () {
-        slider.goToNextSlide();
-       });
-       // "이전" 버튼 클릭시 동작 정의
-       $('.bx-prev').click(function () {
-        slider.goToPrevSlide();
-       });
+   $('.bx-next').click(function () {
+    slider.goToNextSlide();
+   });
+   // "이전" 버튼 클릭시 동작 정의
+   $('.bx-prev').click(function () {
+    slider.goToPrevSlide();
+   });
 }
 
 // 사이드랩 메인메뉴  클릭이벤트
@@ -36,11 +34,17 @@ function menuOpen() {
       }
       else{  
           $(".side_wrap").addClass("on");
-          $(".wrap").css ({"left":"-390px"})
+          $(".wrap").css ({"left":"-250px"})
       }
     
   });
 }
+
+ //비행기가 보일때 안보일때로 
+
+//btn_all을 클릭했을때 사이드랩을 보여줘라
+  
+//해당 리스트를 클릭했을때 on클래스를 추가해주고 텍스트효과와 비행기를 보여줘라
 
 function airplaneGo(){
   $(".nav .gnb>li>a").on("click",function (){
@@ -56,43 +60,27 @@ function airplaneGo(){
 
 
       $(".nav .gnb>li").parent().children().removeClass("on");
-       $(".nav .gnb>li>a").find(".sp_img").removeClass("on");
+      $(".nav .gnb>li>a").find(".sp_img").removeClass("on");
          
  
-         $(this).parent().addClass("on");
+      $(this).parent().addClass("on");
 
-          $(this).find(".sp_img").addClass("on");
+      $(this).find(".sp_img").addClass("on");
 
   })
 }
 
-function sub_imgOn(){
-  $(".nav .gnb>li").on("click",function(){
 
-       var index = $(this).find(".sp_img").index();
-        
-   if ($(this).hasClass("on")) {
-       $(".sp_img").removeClass("on");
-          
-      }
-    else{
-         
-        $(this).eq(index).addClass("on");
-    } 
-  })
+function quickNav() {
+  $(".quick_nav li>a").on("click focusin", function () {
 
+    $(".quick_nav li").parent().children().removeClass("on");
+    $(".quick_nav li>a").find(".sp_img2").removeClass("on");
+
+    $(this).parent().addClass("on")
+    $(this).find(".sp_img2").addClass("on");
+  });
 }
-
-  
-     
-
-
-    
-  //비행기가 보일때 안보일때로 
-
-//btn_all을 클릭했을때 사이드랩을 보여줘라
-  
-//해당 리스트를 클릭했을때 on클래스를 추가해주고 텍스트효과와 비행기를 보여줘라
  
 
 
