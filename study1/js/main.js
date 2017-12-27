165b2d6e8920b42c517a5403ad803f14263dc84a
CHO-PC\CHO <tomorrowcho@gmail.com>
2017-12-28 04:51:21 +0900

    171228_0451 으헝 구조만 짠

--- study1/js/main.js
+++ study1/js/main.js
@@ -54,7 +54,10 @@ function searchClose(){
 // 탭
 function tab() {
 	$(".tab>li>a").on("click focusin", function () {
-	 $(this).parent().addClass("on").siblings().removeClass("on");
+   $(this).parent().addClass("on").siblings().removeClass("on");
+   // $(this).parent().addClass("on")
+   // $(this).parent().siblings().removeClass("on");
+	
 	});
 }
 
