2e42f4491509147561891f397dc7ba5eba9c6e96
tomorrowcho <yoonu78@naver.com>
2018-01-10 00:43:59 +0900

    180110_1243 이제 메롱메롱거리진 않아요

--- study5/js/main.js
+++ study5/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   	gnbOpen();
     bxSlider();
+    btnOpen();
     //subClose();
   	//searchOpen();
     //searchClose();
@@ -11,11 +12,11 @@ $(document).ready(function(){
 
 function gnbOpen(){
   $(window).resize(function (){
-    var windowWidth = $( window ).width();
+    var windowWidth = $(window).width();
       if(windowWidth >=768) {
          //창 가로 크기가 767보다 클경우
           // 헤더
-          $(".gnb").css({"display":"block"})
+          $(".gnb.on").css({"display":"block"})
           $(".gnb > li>a").on("mouseleave", function () {
             $(this).find(".sub_wrap").stop().slideUp(300);
 
@@ -29,7 +30,7 @@ function gnbOpen(){
           // 탭키이동  서브메뉴 마지막이벤트 
           $(".gnb li:last-child .sub_wrap li:last-child>a").on ("focusout",function(){
             $(this).parents("li").removeClass("on");
-          })
+          });
           // 퀵메뉴
           $(".quick_nav li>a").on("click focusin", function () {
 
@@ -52,19 +53,76 @@ function gnbOpen(){
           })
       }else{
          //창 가로 크기가 767 미만일 경우 
-        $(".gnb").css({"display":"none"})
-        $(".btn_all>a").on("click", function () {
-          // $(this).addClass("on").siblings().removeClass("on");
-          $(this).parent().next().slideDown();
-          $(".btn_all>a").parent().next().slideUp();
+        // $(".gnb").css({"display":"none"})
+        // $(".btn_all>a").on("click keyup", function () {
+        //    $(this).toggleClass("on").parent().next().slideToggle();
            
-        });
-      
+            // if($(this).hasClass("on")){
+            //     $(this).removeClass("on").parent().next().slideUp();
+            // }
+            // else{
+            //     $(this).addClass("on").parent().next().slideDown();
+            // } 
+              // $(".gnb").css({"display":"none"})
+              // $(".gnb.on").css({"display":"block"})
+          // //버튼을 클릭했을때 on클래스제거 gnb도 제거 리스트 숨겨주거나 슬라이드업
+          // $(".btn_all>a").removeClass("on")
+          // $(".btn_all>a").parent().parent().find(".gnb").removeClass("on")
+          // $(".gnb").css({"display":"none"})
+
+          // //버튼을 클릭했을때 on클래스추가 gnb도 on 리스트보여주거나 슬라이드 다운
+          // $(this).addClass("on")
+          // $(this).parent().parent().find(".gnb").addClass("on")
+          // $(".gnb").css({"display":"block"})
+
+           //if문 사용시
+           // if(!$(this).hasClass("on")){
+           //   $(this).addClass("on")
+           //    $(this).parent().next().addClass("on")
+           //    $(".gnb.on").css({"display":"block"})
+           // }
+           // else{
+              
+           //    $(".btn_all>a").removeClass("on")
+           //    $(".btn_all>a").parent().next().removeClass("on")
+           //    $(".gnb").css({"display":"none"})
+           // }
+
+          // $(this).addClass("on");
+          // $(this).removeClass("on");
+          //  $(".btn_all>a").parent().next().removeClass("on");
+          //  $(this).parent().next().addClass("on");
+          // $(this).toggleClass("on").parent().next().slideToggle();
+          // $(".btn_all>a").parent().next().slideUp();
+          // $(".gnb").css({"display":"block"})
+
+          // if문 사용시
+           // if($(this).hasClass("on")){
+           //    $(".btn_all>a").removeClass("on");
+           //    $(".btn_all>a").parent().next().slideUp();
+           //    // $(this).removeClass("on");
+           //    // $(".gnb").css({"display":"none"})
+           // }
+           // else{
+           //    $(this).addClass("on").parent();
+           //     $(this).parent().next().slideDown();
+           //     // $(this).addClass("on");
+           // }
+           
+          // }); 
       }
-     
   }).resize();  
 }
 
+
+function btnOpen(){
+  $(".gnb").css({"display":"none"})
+  // $(".gnb.on").css({"display":"block"})
+   $(".gnb").removeClass("on");     
+  $(".btn_all>a").on("click keyup", function () {
+    $(this).toggleClass("on").parent().next().slideToggle();
+   });        
+}
 // bxslider 롤링
 function bxSlider(){
   var slider= $(".bxslider").bxSlider({
