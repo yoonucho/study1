21b8bc9f90cc12c0b00bf01f9fd7e710ba60be06
tomorrowcho <yoonu78@naver.com>
2018-01-23 17:56:45 +0900

    인풋개수 3개제한으로 제이쿼리수정후 완

--- study6/js/main.js
+++ study6/js/main.js
@@ -9,15 +9,54 @@ $(document).ready(function(){
 
 function btnAdd(){
 	var lst= $(".section4 ul");
-	var input_wrap = "<li>";
-			input_wrap +=("<div class='input_wrap'><input type='text' placeholder='http://'></div>");
-			input_wrap +="<li>";
+	var length = 1;
+	var input_wrap = "<li>"+"<div class='input_wrap'><input type='text' placeholder='http://'></div>"+"<li>";
+			// input_wrap +=("<div class='input_wrap'><input type='text' placeholder='http://'></div>");
+			// input_wrap +="<li>";
 	$(".btn_add").on("click", function(){
-		// alert("test");
-		lst.append(input_wrap);
+		// alert(lst.append(input_wrap).length)
+		if(length==3){
+			alert("최대 3개까지 입력가능합니다!")
+			return false;
+		}else{
+			lst.append(input_wrap)
+			length++;
+		}
+		// lst.append(input_wrap).length;
 	return false;
 	});
 }
+
+// function AddUrl(){
+	
+// 	var urlwrap = $('.lst_inputform');
+// 	var urllength = 1;
+	
+// 	$('.loc_btn_add').on('click', function(){
+// 		var urlnode =	'<li>';
+// 		urlnode	+=	'<input type="text" placeholder="http://" id="sns_url' + (urllength+1) + '" name="sns_url' + (urllength+1) + '" style="width:824px" title="URL 입력" data-login-yn="Y" login-layer-top="3500">';
+// 		urlnode	+=	'</li>';
+
+// 		if(urllength == 3){return;}
+// 		urlwrap.append(urlnode);
+// 		urllength++;
+// 	});
+	
+// }
+
+// 대표님 소스
+// function btnAdd(){
+// 	var lst= $(".section4 ul");
+// 	var input_wrap = "<li>";
+// 			input_wrap +=("<div class='input_wrap'><input type='text' placeholder='http://'></div>");
+// 			input_wrap +="<li>";
+// 	$(".btn_add").on("click", function(){
+// 		// alert("test");
+// 		lst.append(input_wrap);
+// 	return false;
+// 	});
+// }
+
 
 
 // function AddUrl(){
