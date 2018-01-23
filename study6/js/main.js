ee4bf49eabf1e0743195a6dc1b9b34fc43a38989
tomorrowcho <yoonu78@naver.com>
2018-01-23 15:48:35 +0900

    180123_1548 append추가후 끗!

--- study6/js/main.js
+++ study6/js/main.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
   	imageSlider();
-  	//tab();
+  	btnAdd();
 });
 
 
 
 
+
+function btnAdd(){
+	var lst= $(".section4 ul");
+	var input_wrap = "<li>";
+			input_wrap +=("<div class='input_wrap'><input type='text' placeholder='http://'></div>");
+			input_wrap +="<li>";
+	$(".btn_add").on("click", function(){
+		// alert("test");
+		lst.append(input_wrap);
+	return false;
+	});
+}
+
+
+// function AddUrl(){
+//    var urlwrap = $('.lst_inputform');
+//    var urlnode = '<li>';
+//       urlnode   += '<input type="text" value="http://" style="width:824px" title="URL 입력">';
+//       urlnode += '<li>';
+//    $('.loc_btn_add').on('click',function(){
+//       urlwrap.append(urlnode);
+//    });
+// }
 
 
 
